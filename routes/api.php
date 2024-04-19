813231ab9f5d50a6c6c986c9351bac20e5ce8cdf
RamirezJosue <xdemobtkx@gmail.com>
2024-04-19 17:34:33 -0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\common\CommonController;
+use App\Http\Controllers\Salud\OdontogramaController;
 
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
@@ -13,5 +14,12 @@ Route::prefix('auth')->group(function () {
 Route::group(['middleware' => ['auth:api']], function(){
     Route::get('profile', [AuthController::class, 'profile']);
     Route::get('logout', [AuthController::class, 'logout']);
+
+    Route::prefix('salud')->group(function () {
+        Route::get('teeth/{tipoodontogramaid}', [OdontogramaController::class, 'getTeeth']);
+        Route::get('numero-pieza/{tipoodontogramaid}', [OdontogramaController::class, 'getPiezas']);
+        Route::get('face-type', [OdontogramaController::class, 'getFaceType']);
+        Route::get('type-treatment', [OdontogramaController::class, 'getTypeTreatment']);
+    });
 });
 
